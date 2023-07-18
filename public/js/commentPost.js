b7f7eeeb2fb7f7f5ebe28965c7d60ba3320b0546
guymorganb <guymorganb@gmail.com>
2023-07-17 23:50:57 -0500

    working through the button controls

--- public/js/commentPost.js
+++ public/js/commentPost.js
@@ -1,35 +1,69 @@
 /**
  * posts a comment onto the blog thread
  */
-let commentBtn = document.getElementById('btn-comment')
+let commentBtn = document.querySelectorAll('.btn-comment');
 
 
-const init = () =>{
+const init = () => {
     document.addEventListener('DOMContentLoaded', ()=>{
         commentBtn.addEventListener('click', (event)=>{
+            
+            
+            const buttonId = event.target.dataset.id;
             if (event.target.classList.contains('btn-comment')) {
-                // Get the post container of the clicked button
-                const postContainer = event.target.closest('.post-container');
-    
-                // Check if the comment box already exists
-                const commentBox = postContainer.querySelector('.comment-box');
-                const commentBoxes = document.querySelectorAll('.comment-box');
-                
-                // If there are any comment boxes, remove them
-                commentBoxes.forEach(box => box.remove());
-                // If the comment box exists, remove it
-                // if (commentBox) {
-                //     commentBox.remove();
-                // } else {
-                //     // If the comment box doesn't exist, create it
-                //     const textarea = document.createElement('textarea');
-                //     textarea.classList.add('comment-box');
-                    
-                //     // Append the textarea to the post container
-                //     postContainer.appendChild(textarea);
-                // }
-            }
 
-        })
-    })
+                const postContainer = document.getElementById('userComments');
+                
+
+                // Create a string with your HTML
+                let cloneString = `
+                    <div style="min-height: 50px; width: auto;" id="userComments" data-aos="fade-left" data-aos-delay="0" data-aos-duration="1500" data-aos-once="true" class="col-md-12 text-center post-container" style="background: gray; display: flex; justify-content: space-between; align-items: flex-start;">
+                        <div style="min-height: 50px; width: auto;" class="post-comment-container">
+                            <div id="comments" style="width: auto; min-height: 50px; overflow: visible;" class="comment-content" contentEditable="true"></div>
+                            <span class="comment-author"></span>
+                        </div>
+                        <button id="submitBtnElem" class="btn btn-submit">Submit</button>
+                    </div>`;
+                // Convert the string to a DOM node
+                const tempNode = document.createElement('div');
+                tempNode.innerHTML = cloneString.trim();
+                const clone = tempNode.firstChild;
+                // Set the initial width and transition properties on the cloned postContainer
+                clone.style.width = "30%";
+                clone.style.height = "30%"; // start at 50% width
+                clone.style.transition = "all 1s ease-in-out";
+                // Append the cloned postContainer after the original postContainer
+                postContainer.parentNode.insertBefore(clone, postContainer.nextSibling);
+                // After a short delay, change the width to 100% to see the transition
+                setTimeout(() => {
+                clone.style.width = "100%";
+                clone.style.height = "100%";
+                }, 100);  // change width after 100ms
+                // Create CKEditor instance for the new comment content area
+                let editor;
+                if (CKEDITOR.instances.postContent) {
+                    editor = CKEDITOR.instances.postContent;
+                } else {
+                    editor = CKEDITOR.replace(`comments`);
+                }
+                let submitBtn = document.getElementById('submitBtnElem')
+                let postContent = editor.getData();
+                editor.on( 'change', function( evt ) {
+                    postContent = evt.editor.getData();
+                });
+                submitBtn.addEventListener('click', ()=>{
+                    console.log('click')
+                })
+    
+           
+    
+                // Remove the cloned element after 5.5 seconds
+                setTimeout(() => {
+                    postContainer.parentNode.removeChild(clone);
+                }, 5500);
+            }
+        });
+    });
 }
+
+init();
